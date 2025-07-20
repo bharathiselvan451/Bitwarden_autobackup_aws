7d2c480cee514f54011d75d791909673d6cfc61a
Bharathiselvan Rajendran <107631601+bharathiselvan451@users.noreply.github.com>
2025-07-20 14:32:31 -0600

    Update exe.sh

--- exe.sh
+++ exe.sh
@@ -10,8 +10,8 @@ sudo apt update && apt install -y curl unzip libsecret-1-0 jq
 export VER=$(curl -H "Accept: application/vnd.github+json" https://api.github.com/repos/bitwarden/clients/releases | jq  -r 'sort_by(.published_at) | reverse | .[].name | select( index("CLI") )' | sed 's:.*CLI v::' | head -n 1) && \
 curl -LO "https://github.com/bitwarden/clients/releases/download/cli-v{$VER}/bw-linux-{$VER}.zip" \
 && unzip *.zip && chmod +x ./bw
-./bw login --apikey
+./home/ubuntu/bw login --apikey
 expect -c "
-spawn ./bw export --output /home/ubuntu/exe.json --format json
+spawn ./home/ubuntu/bw export --output /home/ubuntu/exe.json --format json
 expect -nocase \"password:\" {send \"$1\r\"; interact}
 "
